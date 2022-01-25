e1cd14e661e37459e2ec5bba0e1bd9d7dfbab4ea
Laurynas <laurynas.tam@outlook.com>
2022-01-25 09:51:16 +0200

    Add hooks to Register and Login forms

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,18 @@
 import { useRef } from "react";
 import "./login.css"
+import { loginCall } from "../../apiCalls";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@material-ui/core";
 
 export default function Login() {
-  const email = useRef();
+const email = useRef();
   const password = useRef();
+  const {user, isFetching, error, dispatch} = useContext(AuthContext);
+
   const handleClick = (e) => {
     e.preventDefault();
+    loginCall({email: email.current.value, password: password.current.value}, dispatch);
   }
 
   return (
@@ -24,9 +31,13 @@ export default function Login() {
                 placeholder="Email" ref={email} required />
               <input type="Password" type="password" className="loginInput" 
                 placeholder="Password" ref={password} required minLength="6" />
-              <button className="loginButton">Log in</button>
+              <button className="loginButton" type="submit" disabled={isFetching}>
+                {isFetching ? <CircularProgress color="white"/> : "Log In"}
+              </button>
               <span className="loginForgot">Forgot Password?</span>
-              <button className="loginRegisterButton">Create a new account</button>
+              <button className="loginRegisterButton" type="submit" disabled={isFetching}>
+                {isFetching ? <CircularProgress color="white"/> : "Create a new account"}
+              </button>
             </form>
           </div>
         </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,34 @@
-import "./register.css"
+import { useRef } from "react";
+import "./register.css";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (passwordAgain.current.value !== password.current.value) {
+      password.current.setCustomValidity("Passwords don't match");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      }
+      try {
+      await axios.post("auth/register", user);
+      navigate("/login");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,16 +39,16 @@ export default function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input type="Username" className="loginInput" placeholder="Username" />
-            <input type="Email" className="loginInput" placeholder="Email" />
-            <input type="Password" className="loginInput" placeholder="Password" />
-            <input type="Repeat Password" className="loginInput" placeholder="Repeat Password" />
-            <button className="loginButton">Sign up</button>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input type="Username" required ref={username} className="loginInput" placeholder="Username" />
+            <input type="Email" type="email" required ref={email} className="loginInput" placeholder="Email" />
+            <input type="Password" type="password" minLength="6" required ref={password} className="loginInput" placeholder="Password" />
+            <input type="Repeat Password" type="password" required ref={passwordAgain} className="loginInput" placeholder="Repeat Password" />
+            <button className="loginButton" type="submit" >Sign up</button>
             <button className="loginRegisterButton">
               Log into Account
               </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
